773ad46b7d75c143c77c74e6dcaeafb633614586
bart-mant-io <antczak.bar@gmail.com>
2019-08-24 15:51:22 +0200

    Instalacja reduxa, dodatnie reducera dla meals

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { useScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+/* REDUX */
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import mealsReducer from './store/reducers/meals';
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 useScreens();
 
@@ -27,5 +37,5 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  return <Provider store={store}><MealsNavigator /></Provider>;
 }
